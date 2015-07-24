82a55f901f7c31043bb5adedd9cb5476d3fc40a0
breadknock <brenoc@rpi.edu>
2015-07-24 00:50:05 -0400

    Add bbox skeleton for part 30

---
+++ MD4.h
@@ -0,0 +1,7 @@
+#include "DataStream.h"
+
+void iterate_md4_hash(const DataStream &message, std::vector<uint32_t> &hashes);
+
+DataStream md41_hash(const DataStream &message);
+
+DataStream md4_hash_with_key(DataStream key, const DataStream &message);
