69880dc3a269e13ebdccbcb08630693c7d804cee
Christopher Brenon <brenonc@ainfosec.com>
2015-07-31 14:14:07 -0400

    Implement RSA and test in part 39

---
+++ RSA.cc
@@ -0,0 +1,66 @@
+#include "RSA.h"
+#include "DataStream.h"
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/miller_rabin.hpp>
+#include <utility>
+#include <iostream>
+using boost::multiprecision::cpp_int;
+
+
+RSAClient::RSAClient(int bit_size, int _e) {
+    e = _e;
+    cpp_int p = gen_prime(bit_size, _e);
+    cpp_int q = gen_prime(bit_size, _e);
+    n = p * q;
+    cpp_int toit = (p - 1) * (q - 1); // since (p,q) are prime
+    do {
+        d = inv_mod(e, toit);
+        if(!d) e++;
+    } while (d == 0);
+    std::cout<<e<<std::endl;
+}
+
+RSA_key RSAClient::get_public_key() {
+    return std::make_pair(e, n);
+}
+
+DataStream RSAClient::decrypt(const DataStream &cipher) {
+    return DataStream(powm(cipher.getCppInt(), d, n));
+}
+
+DataStream encrypt(const DataStream &ds, const RSA_key &key) {
+    return DataStream(powm(ds.getCppInt(), key.first, key.second));
+}
+
+cpp_int inv_mod(const cpp_int &num, const cpp_int &mod) {
+    cpp_int r0 = mod, r1 = num;
+    cpp_int t0 = 0, t1 = 1;
+    while(r1 != 0) {
+        cpp_int q = r0 / r1;
+        cpp_int r2 = r0 - q * r1;
+        cpp_int t2 = t0 - q * t1;
+        r0 = r1;
+        r1 = r2;
+        t0 = t1;
+        t1 = t2;
+    }
+    if(r0 > 1) {
+        return 0;
+    }
+    return (t0 + mod) % mod;
+}
+
+
+cpp_int gen_prime(int bit_size, int no_div) {
+    cpp_int prime;
+    do {
+        if(no_div > 0) {
+            do {
+                prime = get_random_key(bit_size / 8).getCppInt();
+            } while ((prime - 1) % no_div == 0);
+        } else {
+            prime = get_random_key(bit_size / 8).getCppInt();
+        }
+    } while(!miller_rabin_test(prime, 100));
+    return prime;
+}
---
+++ RSA.h
@@ -0,0 +1,27 @@
+#include <boost/multiprecision/cpp_int.hpp>
+#include "DataStream.h"
+#include <utility>
+using boost::multiprecision::cpp_int;
+
+typedef std::pair<cpp_int, cpp_int> RSA_key;
+
+class RSAClient {
+private:
+    cpp_int e;
+    cpp_int d;
+    cpp_int n;
+
+public:
+    RSAClient(int bit_size=1024, int e=3);
+
+    RSA_key get_public_key();
+
+    DataStream decrypt(const DataStream &ds);
+
+};
+
+DataStream encrypt(const DataStream &ds, const RSA_key &key);
+
+cpp_int inv_mod(const cpp_int &num, const cpp_int &mod);
+
+cpp_int gen_prime(int bit_size, int no_div=0);
